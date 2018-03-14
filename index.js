dad42e709c1394e16bbbd83e56bcbc07091b38d0
Colter-Hammer <therondowdle7@gmail.com>
2018-03-13 18:32:37 -0600

    figured it out eventually

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var fs = require('fs');
 
 function readFileGetWord(callback) {
 
-    // When the function is first declared, it runs this console.log() though I don't completely understand why.
+    // When the function is first declared, it runs this console.log() though I don't completely understand why
+    //it's running up here when the function is being assigned to a variable.
     console.log(3);
+    
     fs.readFile('data.json', 'utf8', function (err, fileString) {
-        console.log(0);
+
+        // It'll check this once the readFile is done running, but before doing the callback.
+        console.log(13);
         if (err) {
             callback(err);
             // if this returned a value where would it go?
@@ -16,7 +20,7 @@ function readFileGetWord(callback) {
         var words = callback(null, data[0]);
         console.log("Value returned from callback inside readFileGetWord:", words);
 
-        //Because it's async, the other things continue running while this function is run. When this function is done running, the other things have already finished so this is last.
+        //
         console.log(16);
 
         //where does this string go?
@@ -33,11 +37,15 @@ function readFileGetWord(callback) {
 }
 
 function addNumbers(a, b, callback) {
+
+    // first thing that happens when calling the function
     console.log(6);
 
     var notANumber = callback(null, a + b);
     console.log("Value returned from callback in addNumbers:", notANumber);
-    console.log(0);
+
+    // The callback is called, so that function is run, and after this one finished since there is additional information. 
+    console.log(8);
     //it is weird for the "node-pattern of handling async problems" to return something (don't do this) 
     //but I want you to think about this to fully understand async flow of execution
     //and to understand the difference between an async function and a callback
@@ -52,8 +60,11 @@ function start() {
     console.log(2)
 
     // if the return was used, text = 'return';
+    // Why is it calling/running readFileGetWord when it's being assigned to a variable?
     text = readFileGetWord(function (err, word) {
-        console.log(0);
+        
+        
+        console.log(14);
 
         if (err) {
             console.log(err);
@@ -62,13 +73,16 @@ function start() {
         }
 
         console.log("Word from file:", word);
-        console.log(0);
+
+        // I know the callback has been called because of the console.log("word.....")
+        // Because of that, I know that this will be right after that.  
+        console.log(15);
 
         //this return is also weird, just want you to think about it
         return "this is weird";
     })
 
-    // readFileGetWord starts running it's async function, and moves on.
+    // readFileGetWord starts running its async function, and moves on.
     console.log(5);
     number = addNumbers(2, 3, function (err, sum) {
         if (err) {
@@ -76,15 +90,15 @@ function start() {
             //if this returned a value where would it go?
             return;
         }
-        console.log(0);
+        console.log(7);
         console.log("Sum:", sum);
         //this return is also weird, just want you to think about it
         return "not a number";
     });
 
-    console.log(0);
+    console.log(9);
     console.log("Value returned from addNumbers:", number);
-    console.log(0);
+    console.log(10);
     console.log("Value returned from readFileGetWord:", text);
     console.log(0);
 }
